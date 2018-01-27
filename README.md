4ee923129714f2d7cf02f621b665396721601d27
NoListen <listenonepiece@gmail.com>
2018-01-27 19:36:52 +0000

    add comments in the env & add instruction

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Modification
 It has been adapted to [Torchcraft v 1.3.0](https://github.com/TorchCraft/TorchCraft/releases) and [BWAPI v 4.1.2](https://github.com/bwapi/bwapi/releases). It doesn't need torchcraft-py anymore because [Torchcraft v 1.3.0](https://github.com/TorchCraft/TorchCraft/releases) supports python API.
 
+# Compound Battel Env
+
+The observations are returned as one dictionary.
+
+- ul     unit location.
+- ud     unit data
+- au     number of alive units
+- mask   [1, 1, 1, 0, 0] 3 alive among 5 units.
+- s      2D scene map including health, shield, type, flag, unit data [used for convolution network]
+
+Two types of rewards
+
+- total reward for one camp (Set UNIT_REWARD to False)
+- unit reward for each unit (Set UNIT_REWARD to True)
+
+# BiCNet
+
+It's compatitle with the algorithm in [multi-ddpg](https://github.com/NoListen/RL-forest/tree/master/RL_forest/ddpg_plant/multi_ddpg).
+
 # gym-starcraft
 Gym StarCraft is an environment bundle for OpenAI Gym. It is based on [Facebook's TorchCraft](https://github.com/TorchCraft/TorchCraft), which is a bridge between Torch and StarCraft for AI research.
 
--- gym_starcraft/envs/compound_battle_env.py
+++ gym_starcraft/envs/compound_battle_env.py
@@ -9,38 +9,50 @@ import math
 # used to draw the map
 import cv2
 
+# distance each distance action represents
 DISTANCE_FACTOR = 25
+# numeber of self-camp numbers
 MYSELF_NUM = 3
+# number of enemy-camp numbers
 ENEMY_NUM = 1
+# the length of each unit's feature
 DATA_NUM = 8
+# the cropped map size
 MAP_SIZE = 40
+# the scale applied to unit's health
 HEALTH_SCALE = 20.
+# time scale to unit's CD
 TIME_SCALE = 10.
+# attack the enemy based on the target place. Auto attack otherwise.
 COMPLICATE_ACTION = True#False
+# Used for Dynamic-RNN. All alive units occupy the first several elements.
 DYNAMIC = False # Dynamic means compression
+# won't go a long way to attack a far away enemy.
+# otherwise result in large variance
 TARGET_VAR = 1000000
+# each unit has its own reward
 UNIT_REWARD = True
+# calculate the unit reward from the closest 4 units
 K = 4
+# the feature about the each enemy
 ENEMY_DATA_NUM = 2
+# prevent the soldiers out of the limited zone because of inertia
 MARGIN = 10
-ENEMY_COMMAND = True
+
 # 96 by 96
 # static map at first
-#CROP_LT = (10, 10)
+# the left-top of the cropped area
 CROP_LT = (60, 120)
-#CROP_RB = (240, 240)
+# the right-bottom of the cropped area
 CROP_RB = (100, 140)
-# times relative to the map
+
 SCALE = (max(CROP_RB[1] - CROP_LT[1], CROP_RB[0] - CROP_LT[0]))/float(MAP_SIZE)
-# The range is kind of cheating.
-# So data had better only include 1.health channel 3
-#  2.shield, 3
-# 3. enemy (flag). or player 3
-# 4. type 3
-# 5. Where is myself. 1
 
 # one image with 13 channels.
 # TODO: How one AI robot learns to learn his enemy, by something like transferring the viewpoints.
+
+# 10 and 5 are experical parameters
+# used for drawing the scene on a picture
 def pixel_coordinates(unit):
     x0 = unit.x - unit.pixel_size_x/10. - CROP_LT[0]
     y0 = unit.y - unit.pixel_size_y/10. - CROP_LT[1]
@@ -48,7 +60,8 @@ def pixel_coordinates(unit):
     y1 = y0 + unit.pixel_size_y/5.
     return (int(x0/SCALE), int(y0/SCALE)), (int(x1/SCALE), int(y1/SCALE))
 
-
+# construct the map
+# the health or something else need to be represented in RGB channels
 def get_map(map_type, unit_dict_list):
     # unit locations need to be drawed into different maps.
     if map_type == "unit_location" or "unit_density":
@@ -77,9 +90,6 @@ def compute_cos_value(unit_v1, v2):
     cos_value = v_product/np.linalg.norm(v2)
     return cos_value
 
-# I considers that in this situation, the neural network will learn the mapping rather
-# than the relationship between positions and angles.
-
 # this data needs to be regularized
 # Not consider the air temporally
 def extreme(cor, ext_cor):
@@ -92,37 +102,34 @@ def extreme(cor, ext_cor):
 def resize_map(map_cor):
     center = (int((map_cor[0] + map_cor[2])/2.), int((map_cor[1] + map_cor[3])/2.))
     range  = max(map_cor[2]-map_cor[0], map_cor[3]-map_cor[1])
-#    print(range, map_cor)
     scale = range/float(MAP_SIZE) #(I still want a circle rather than an oval)
 
     return center, range, scale
 
 class data_unit(object):
     def __init__(self, unit, id):
-        #print unit.groundRange, "Range"
-        self.id = id
-        self.health = unit.health
-        self.x = unit.x
-        self.y = unit.y
-        self.shield = unit.shield
-        self.attackCD = unit.groundCD
-        # the type is encoded to be one-hot, but is not considered temporally
-        self.type = unit.type
-        self.groundATK = unit.groundATK
-        self.groundRange = unit.groundRange # this can be inferred from training
-        self.under_attack = unit.under_attack
-        self.attacking = unit.attacking
-        self.moving = unit.moving
-        #TODO maintain a TOP-K list
-        self.die = False
-        self.max_health = unit.max_health
-        self.max_shield = unit.max_shield
-        self.pixel_size_x = unit.pixel_size_x
-        self.pixel_size_y = unit.pixel_size_y
+        self.id = id                          # unit id
+        self.health = unit.health             # health
+        self.x = unit.x                       # x
+        self.y = unit.y                       # y
+        self.shield = unit.shield             # shield
+        self.attackCD = unit.groundCD         # attackCD
+        self.type = unit.type                 # type
+        self.groundATK = unit.groundATK       # Attack power
+        self.groundRange = unit.groundRange   # this can be inferred from training
+        self.under_attack = unit.under_attack # Boolean. whether is under attacking.
+        self.attacking = unit.attacking       # Boolean. whether is attacking.
+        self.moving = unit.moving             # Boolean. whether is moving.
+        self.die = False                      # Boolean. alive.
+        self.max_health = unit.max_health     # max health of this type of unit.
+        self.max_shield = unit.max_shield     # max shield of this type of unit.
+        self.pixel_size_x = unit.pixel_size_x # pixel_size of this type of uni
+        self.pixel_size_y = unit.pixel_size_y # pixel_size on this type of uni
         self._delta_health = 0
 
     def update_data(self, unit):
-        self._delta_health = self.health - unit.health
+        # other data remain the same
+        self._delta_health = self.health - unit.health # used to calculate the reward
         self.health = unit.health
         self.x = unit.x
         self.y = unit.y
@@ -131,10 +138,10 @@ class data_unit(object):
         self.under_attack = unit.under_attack
         self.attacking = unit.attacking
         self.moving = unit.moving
-        #if self.health <= 0: # die
         self.die = False
 
     def in_map(self):
+        # whether stay in the map
         if self.x >= CROP_LT[0]-MARGIN and self.y >= CROP_LT[1]-MARGIN and self.x <= CROP_RB[0]+MARGIN and self.y <= CROP_RB[1]+MARGIN:
             return True
         return False
@@ -143,24 +150,22 @@ class data_unit(object):
     def delta_health(self):
         return self._delta_health
 
-
     def extract_data(self, unit_dict):
-        # type not included
         if self.die:
-            # still communicate but hope skip this one. ( convenient for experience store and replay )
-            # I am afraid there will be some memory leakage using the object.
             #return [100, 140, 0, 0, 0, self.groundATK/HEALTH_SCALE, self.groundRange, 0, 0, 0]+[100, 1, 0, 0]*ENEMY_NUM, 0.
             #return [0, 0, 0, self.groundATK/HEALTH_SCALE, self.groundRange, 0, 0, 0]+[100, 1, 0, 0]*ENEMY_NUM, 0.
+            # [health, shield, attackCD, groundATK, groundRange, under_attack, attacking, moving]
             return [ 0,0, 0, self.groundATK/HEALTH_SCALE, self.groundRange, 0, 0, 0]+[-1, 1]*ENEMY_NUM, 0.
 
+        # elimate x, y to prevent overfitting
         #data = [self.x, self.y, self.health/HEALTH_SCALE, self.shield/HEALTH_SCALE, self.attackCD/TIME_SCALE, self.groundATK/HEALTH_SCALE, self.groundRange,
         data = [self.health/HEALTH_SCALE, self.shield/HEALTH_SCALE, self.attackCD/TIME_SCALE, self.groundATK/HEALTH_SCALE, self.groundRange,
                  self.under_attack, self.attacking, self.moving]
         assert(len(data) == DATA_NUM)
         data += unit_dict.get_distance_degree(self)
-        #data = [self.x, self.y, self.health / HEALTH_SCALE, self.shield / HEALTH_SCALE, self.under_attack, self.attacking, self.moving]
         return data, 1.
 
+    # mask this unit in multi-agent setting
     def extract_mask(self):
         if self.die:
             return 0.
@@ -182,10 +187,10 @@ class data_unit_dict(object):
         self.id_list = sorted(self.units_dict.keys())
         self.alive_num = -1
         self.num = len(self.id_list)
-        #print(self.id_list, "id list")
 
     def update(self, units):
         for id in self.id_list:
+            # if alive, it'll be updated to False later
             self.units_dict[id].die = True
         self.alive_num = len(units)
         for u in units:
@@ -193,24 +198,26 @@ class data_unit_dict(object):
             unit = self.units_dict[id]
             unit.update_data(u)
 
+    # the the degree and distance from one unit
     def get_distance_degree(self, unit):
         dd = []
         for id in self.id_list:
             t = self.units_dict[id]
             distance = utils.get_distance(unit.x, unit.y, t.x, t.y)
             degree = utils.get_degree(unit.x, unit.y, t.x, t.y)/180.
-            #dd += [distance, t.die, t.groundRange, degree]
             dd += [distance, degree]
         return dd
-            
+
 
     def extract_data(self, unit_dict):
         mask_list = np.zeros(self.num, dtype="uint8")
         data_list = np.zeros((self.num, DATA_NUM+ENEMY_DATA_NUM*ENEMY_NUM), dtype="float32")
         i = 0
         for id in self.id_list:
+            # if dynamic RNN, put the alive units' data in the beginning
             if DYNAMIC and self.units_dict[id].die:
                 continue
+            # mask = 1 if alive 0 otherwise
             data_list[i], mask_list[i] = self.units_dict[id].extract_data(unit_dict)
             i += 1
         return data_list, mask_list
@@ -234,10 +241,10 @@ class data_unit_dict(object):
             if unit.die and DYNAMIC:
                 continue
             mask_list[i] = self.units_dict[id].extract_mask()
-            # mask_list[i] = mask
             i += 1
         return mask_list
 
+    # draw the feature maps using opencv
     def get_map(self, map_type, map, idx=None):
         #well, if the map is "unit_location", the color is only one.
         id_index = 0
@@ -278,18 +285,16 @@ class data_unit_dict(object):
             id_index += 1
         return map
 
-    # unit is a foreign unit ( not in this dict )
+    # unit is from another camp ( not in this dict )
     def compute_closest_position(self, unit, action):
         # unit vector
         degree = action[1] * 180
-        # TODO consider to change the target range.
         distance = (action[2] + 1) * unit.groundRange/2.  # at most 2*DISTANCE_FACTOR
         tx, ty = utils.get_position2(degree, distance, unit.x, unit.y)
-        # TODO only consider those within the groundRange
-        # limit the distance between target and target unit
         target_id = self.compute_candidate(tx, ty)
         return target_id
 
+    # choose the closest one
     def compute_candidate(self, tx, ty):
         target_id = None
         # set it to be a big positive integer
@@ -301,12 +306,14 @@ class data_unit_dict(object):
             # TODO if the value is normalized, pay attention to change the value here.
             target_unit = self.units_dict[id]
             td = (target_unit.x - tx)**2 + (target_unit.y - ty)**2
+
             if td < d and td < TARGET_VAR:
                 target_id = target_unit.id
                 d = td
         # None or the enemy
         return target_id
 
+    # calculate the reward specific to each unit
     def compute_single_unit_rewards(self, k, unit_dict):
         idx = 0
         rewards = np.zeros(self.num, dtype="float32")
@@ -320,6 +327,7 @@ class data_unit_dict(object):
             idx += 1
         return np.array(rewards)
 
+    # compute rewards specific to each unit
     def compute_unit_rewards(self, k, unit_dict_list):
         idx = 0
         rewards = np.zeros(self.num, dtype="float32")
@@ -335,7 +343,7 @@ class data_unit_dict(object):
         return np.array(rewards)
 
 
-# by default, use all the maps.
+
 class CompoundBattleEnv(sc.StarCraftEnv):
     def __init__(self, server_ip, server_port, speed=0, frame_skip=0,
                  self_play=False, max_episode_steps=1000,
@@ -343,11 +351,13 @@ class CompoundBattleEnv(sc.StarCraftEnv):
         self.map_types_table = map_types_table
         self.obs_cls = list(set(utils.obs_cls_table[k] for k in self.map_types_table)) + ["mask", "au"]
         """
+        All map types (s excluded by default)
+        
         ul - unit location.
         ud - unit data
         au - number of alive units
         mask - [1, 1, 1, 0, 0] 3 alive among 5 units.
-        s - situation including health, shield, type, flag, unit data
+        s - situation including health, shield, type, flag, unit data [used for convolution network]
         """
         #TODO: au is not returned
 
@@ -390,15 +400,16 @@ class CompoundBattleEnv(sc.StarCraftEnv):
             return (1,)
 
     def _observation_space(self):
-        # unit location
         obs_space = {}
 
+        # unit location
         if "ul" in self.obs_cls:
             ul_low = np.zeros((MYSELF_NUM, MAP_SIZE, MAP_SIZE, 1), dtype=np.uint8)
             ul_high = np.ones((ENEMY_NUM, MAP_SIZE, MAP_SIZE, 1), dtype=np.uint8)
             ul_obs_space = spaces.Box(np.array(ul_low), np.array(ul_high))
             obs_space["ul"] = ul_obs_space
 
+        # 2D scene map / feature map
         if "s" in self.obs_cls:
             global_map_types = [mt for mt in self.map_types_table if ( mt != "unit_location" and mt != "unit_data")]
             map_channels = reduce(lambda x,y: x+y, [utils.map_channels_table[mt] for mt in global_map_types])
@@ -408,6 +419,7 @@ class CompoundBattleEnv(sc.StarCraftEnv):
             map_obs_space = spaces.Box(np.array(map_low), np.array(map_high))
             obs_space["s"] = map_obs_space
 
+        # unity data (numerical)
         if "ud" in self.obs_cls:
             unit_data_low = np.zeros((MYSELF_NUM, DATA_NUM+ENEMY_NUM*ENEMY_DATA_NUM), dtype=np.float32)
             unit_data_low[:,-1] = -1.
@@ -451,16 +463,6 @@ class CompoundBattleEnv(sc.StarCraftEnv):
                 unit_action = action[i]
             cmds += self.take_action(unit_action, unit)
 
-        #if ENEMY_COMMAND:
-        #    for i in range(0, ENEMY_NUM):
-        #        unit = self.enemy_obs_dict.units_dict[i]
-        #        if unit.die:
-        #            continue
-                #myself_id = self.myself_obs_dict.compute_closest_position(unit, [1, 0, 0])
-                #if myself_id:
-                #    print("Attack %i" % myself_id)
-                # cmds.append([tcc.command_unit_protected, unit.id, tcc.unitcommandtypes.Attack_Unit, myself_id])
-         #       cmds.append([tcc.command_unit_protected, unit.id, tcc.unitcommandtypes.Move, -1, 100, 100])
         return cmds
 
     def take_action(self, action, unit):
@@ -476,8 +478,6 @@ class CompoundBattleEnv(sc.StarCraftEnv):
             # TODO: compute the enemy id based on its position ( I DON'T CARE THIS POINT )
                 cmds.append([tcc.command_unit_protected, unit.id, tcc.unitcommandtypes.Attack_Unit, enemy_id])
         else:
-            #print("MOVE", unit.id, uniy.x, unit.y)
-            # Move action
             degree = action[1] * 180
             distance = (action[2] + 1) * DISTANCE_FACTOR  # at most 2*DISTANCE_FACTOR
             x2, y2 = utils.get_position2(degree, distance, unit.x, unit.y)
@@ -491,7 +491,6 @@ class CompoundBattleEnv(sc.StarCraftEnv):
         # the shape needs to be modified.
         # obs = np.zeros(self.observation_space.shape)
 
-        # I'd like to represent the distance in that hot map.
         if self.myself_obs_dict is None:
             self.myself_obs_dict = data_unit_dict(self.state.units[0], 0)
 
@@ -499,19 +498,18 @@ class CompoundBattleEnv(sc.StarCraftEnv):
         if self.enemy_obs_dict is None:
             self.enemy_obs_dict = data_unit_dict(self.state.units[1], 1)
 
-        #TODO center and scale are useless temporally
         self.myself_obs_dict.update(self.state.units[0])
         self.enemy_obs_dict.update(self.state.units[1])
 
-        #assert("unit_location" in self.map_types_table)
         unit_dict_list = [self.myself_obs_dict, self.enemy_obs_dict]
-        #unit_locations = get_map('unit_location', [self.myself_obs_dict]) # 1
         maps = []
 
+        # construct observation dictionary
         obs = {}
         if "ul" in self.obs_cls:
             unit_locations = get_map('unit_location', [self.myself_obs_dict]) # 1
             obs["ul"] = unit_locations
+
         if "s" in self.obs_cls:
             for mt in self.map_types_table:
                 if mt == 'unit_location' or mt == "unit_data":
@@ -519,6 +517,7 @@ class CompoundBattleEnv(sc.StarCraftEnv):
                 maps.append(get_map(mt, unit_dict_list))
             total_maps = np.concatenate(maps, axis=2) # 2
             obs["s"] = total_maps
+
         if "ud" in self.obs_cls:
             obs["ud"], obs["mask"] = self.myself_obs_dict.extract_data(self.enemy_obs_dict)
         else:
@@ -528,12 +527,10 @@ class CompoundBattleEnv(sc.StarCraftEnv):
             obs["au"] = self.myself_obs_dict.alive_num
         else:
             obs["au"] = MYSELF_NUM
-        #assert(obs.keys() == self._observation_dtype.keys())
+
         # TODO normalzie the data in each unit corresponding to the map. PPPPPPPriority HIGH.
         return obs
 
-    # return reward as a list.
-    # I need to know the range at first.
     def _compute_reward(self):
         unit_dict_list = [self.myself_obs_dict, self.enemy_obs_dict]
         if UNIT_REWARD:
